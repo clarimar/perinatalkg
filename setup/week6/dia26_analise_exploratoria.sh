5841c0155173a8541f2954e71ae7e4aa476fd896
Clarimar José Coelho <clarimarc@gmail.com>
2026-05-12 23:57:21 -0300

    Day 26: Exploratory analysis SINASC-SIM linkage variables
    - SINASC GO-2020: 92,585 births, 70 variables
    - SIM GO-2020: 667 neonatal deaths
    - Linkage variables: DTNASC, CODMUNRES, PESO, SEMAGESTAC, IDADEMAE
    - Strategy: blocking + probabilistic scoring

---
+++ setup/week6/dia26_analise_exploratoria.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "DIA 26: Analise exploratoria SINASC-SIM completa"
+echo "Variaveis linkage: DTNASC, CODMUNRES, PESO, SEMAGESTAC, IDADEMAE"
+echo "SIM GO-2020: 667 obitos | SINASC GO-2020: 92,585 nascimentos"
