3d618c8e755b575afb44e045a182ea6926f3c2e4
Clarimar José Coelho <clarimarc@gmail.com>
2026-05-14 14:34:37 -0300

    Day 36: Manuscript figures - architecture, hierarchy, pipeline

--- setup/week8/dia36_figuras.sh
+++ setup/week8/dia36_figuras.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "DIA 36: Figuras manuscrito geradas"
-echo "docs/figures/fig1_architecture.png"
-echo "docs/figures/fig2_class_hierarchy.png"
-echo "docs/figures/fig3_etl_pipeline.png"
+echo "DIA 36: Figuras manuscrito"
+echo "  fig1_architecture.png"
+echo "  fig2_class_hierarchy.png"
+echo "  fig3_etl_pipeline.png"
