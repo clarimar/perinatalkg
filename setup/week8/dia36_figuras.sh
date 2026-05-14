2ef16d49da0cfe04f7be4a2f45b1e6d0b5d238cd
Clarimar José Coelho <clarimarc@gmail.com>
2026-05-14 14:31:30 -0300

    Day 36: Manuscript figures generated
    - Fig 1: System architecture
    - Fig 2: OWL class hierarchy (35 classes)
    - Fig 3: ETL pipeline

---
+++ setup/week8/dia36_figuras.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "DIA 36: Figuras manuscrito geradas"
+echo "docs/figures/fig1_architecture.png"
+echo "docs/figures/fig2_class_hierarchy.png"
+echo "docs/figures/fig3_etl_pipeline.png"
